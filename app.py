91777a0bb8d7430038b2fa6e9ff0fc7d2e4433da
SK-BILAL <136679359+SK-BILAL@users.noreply.github.com>
2025-10-17 23:27:48 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/', methods=['GET'])
 def welcome():
-    return jsonify(message="welcome!, This is Assignment from Better")
+    return jsonify(message="You are welcome!")
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=5000, debug=False)
